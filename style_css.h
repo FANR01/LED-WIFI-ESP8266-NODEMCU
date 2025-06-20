e335d3d2f800d3daa9dc6e20deeab9261e9c3237
FANR01 <83988763+FANR01@users.noreply.github.com>
2025-06-20 14:16:49 +0700

    style_css.h

---
+++ style_css.h
@@ -0,0 +1,26 @@
+const char* css_style = R"====(
+body {
+  font-family: sans-serif;
+  text-align: center;
+  padding-top: 50px;
+  background-color: #f0f0f0;
+}
+button {
+  font-size: 24px;
+  padding: 20px 40px;
+  margin: 15px;
+  border: none;
+  border-radius: 10px;
+  background-color: #4CAF50;
+  color: white;
+  box-shadow: 0px 4px 8px rgba(0,0,0,0.2);
+  transition: 0.2s;
+}
+button:hover {
+  background-color: #45a049;
+}
+button:active {
+  box-shadow: 0 2px 4px rgba(0,0,0,0.2);
+  transform: scale(0.98);
+}
+)=====";
